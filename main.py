895eabcf0c5c8e12fd0a11d373c78bfcaa280388
Tomer_Shaked <tomer1shaked@gmail.com>
2024-04-17 17:56:54 +0300

    merged changes and added utils file for all the small functions

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from torch_geometric.loader import DataLoader
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
-
+from utils import calculate_deleiveries,show_graph_with_labels
 
 class GCN(torch.nn.Module):
     def __init__(self, x_feature_num):
@@ -27,50 +27,9 @@ class GCN(torch.nn.Module):
         return F.log_softmax(x, dim=1)
 
 
-def calculate_deleiveries(A, X):
-    best_routes = np.zeros([len(A), len(A)])
-    # calculate best routes from every source to every destination
-    for i in range(len(A)):
-        # The BFS algorithm calculates the best routes from every starting point. It does this jointly for all
-        # destination nodes using vectorized notation.
-        best_routes[i] = vectorised_BFS(A, i)
-    return np.sum(best_routes * X)
 
 
-def vectorised_BFS(A, start_ind):
-    current_routes = np.zeros(len(A))
-    current_routes[start_ind] = 1
-    pathing_improved = True
-    while pathing_improved:
-        new_routes = best_one_hop(A, current_routes)
-        # accounting for small numerical floating point errors if the new paths are similar to the old path then we have
-        # not improved the paths at all, and we can exit the BFS
-        if ((current_routes[np.arange(len(A)) != start_ind] - new_routes[
-            np.arange(len(A)) != start_ind]) ** 2).sum() < 10 ** (-8):
-            pathing_improved = False
-        current_routes = np.maximum(current_routes, new_routes)
-        # print(current_routes)
-    return current_routes
 
-
-def best_one_hop(A, v):
-    possiblities_mat = A.T * v
-    return np.max(possiblities_mat, axis=1)
-
-
-def show_graph_with_labels(adjacency_matrix):
-    rows, cols = np.where(adjacency_matrix != 0)
-    weighted_edges = [(rows.tolist()[i], cols.tolist()[i], round(adjacency_matrix[rows, cols].tolist()[i], 2)) for i in
-                      range(len(rows.tolist()))]
-
-    gr = nx.Graph()
-
-    gr.add_weighted_edges_from(weighted_edges)
-    pos = nx.spring_layout(gr)
-    nx.draw_networkx(gr, pos)
-    for edge in gr.edges(data='weight'):
-        nx.draw_networkx_edges(gr, pos, edgelist=[edge], width=2 * edge[2] ** 2)
-    plt.show()
 
 
 class delieveries_dataset():
@@ -102,9 +61,11 @@ class delieveries_dataset():
         self.dataset = [0] * self.dataset_size
         for i in range(self.dataset_size):
             rows, cols = np.where(As[i] != 0)
-            edges = [[rows.tolist()[i], cols.tolist()[i]] for i in range(len(rows.tolist()))]
+            edges = [[rows.tolist()[j], cols.tolist()[j]] for j in range(len(rows.tolist()))]
             edges = torch.tensor(edges)
+            weights = [As[i][rows,cols].tolist()[j] for j in range(len(rows.tolist()))]
             self.dataset[i] = Data(x=X, edge_index=edges.t().contiguous())
+            self.dataset[i].weights = weights
 
     def random_adjacency(self, size, sum, edge_precentage):
         mask_matrix = self.random_matrix(size, sym=True)
---
+++ utils.py
@@ -0,0 +1,50 @@
+import numpy as np
+import networkx as nx
+from matplotlib import pyplot as plt
+def calculate_deleiveries(A,X):
+
+    best_routes = np.zeros([len(A),len(A)])
+    # calculate best routes from every source to every destination
+    for i in range(len(A)):
+        # The BFS algorithm calculates the best routes from every starting point. It does this jointly for all
+        # destination nodes using vectorized notation.
+        best_routes[i] = vectorised_BFS(A,i)
+    return np.sum(best_routes*X)
+
+
+
+def vectorised_BFS(A,start_ind):
+    current_routes = np.zeros(len(A))
+    current_routes[start_ind] = 1
+    pathing_improved = True
+    while pathing_improved:
+        new_routes = best_one_hop(A, current_routes)
+        # accounting for small numerical floating point errors if the new paths are similar to the old path then we have
+        # not improved the paths at all, and we can exit the BFS
+        if ((current_routes[np.arange(len(A)) != start_ind] - new_routes[np.arange(len(A)) != start_ind])**2).sum() < 10**(-8):
+            pathing_improved = False
+        current_routes = np.maximum(current_routes, new_routes)
+        # print(current_routes)
+    return current_routes
+
+
+def best_one_hop(A,v):
+     possiblities_mat = A.T * v
+     return np.max(possiblities_mat,axis=1)
+def from_adjacency_tolist(A):
+    rows, cols = np.where(A != 0)
+    edges = [[rows.tolist()[i], cols.tolist()[i]] for i in range(len(rows.tolist()))]
+    weights = [A[rows, cols].tolist()[i] for i in range(len(rows.tolist()))]
+    return edges, weights
+def show_graph_with_labels(adjacency_matrix):
+    rows, cols = np.where(adjacency_matrix != 0)
+    weighted_edges = [(rows.tolist()[i], cols.tolist()[i],round(adjacency_matrix[rows,cols].tolist()[i],2))for i in range(len(rows.tolist()))]
+
+    gr = nx.Graph()
+
+    gr.add_weighted_edges_from(weighted_edges)
+    pos = nx.spring_layout(gr)
+    nx.draw_networkx(gr, pos)
+    for edge in gr.edges(data='weight'):
+        nx.draw_networkx_edges(gr, pos, edgelist=[edge], width=2*edge[2]**2)
+    plt.show()
