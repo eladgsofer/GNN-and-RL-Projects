76e33a79f8c8db299bcc301fa7530482f84a6f95
Elad Sofer <elad.g.sofer@gmail.com>
2024-04-20 16:25:06 +0300

    bugfix batch_size=1

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import networkx as nx
 import tqdm
+import matplotlib.pyplot as plt
 import torch
 
 from torch_geometric.data import Data
@@ -55,7 +56,7 @@ if __name__ == '__main__':
     num_of_nodes = 20
     data_size = 1000
     batch_size = 1
-    epochs = 20
+    epochs = 100
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     D = delieveries_dataset(num_of_nodes=num_of_nodes, dataset_size=data_size, edge_percentage=0.2)
@@ -67,25 +68,34 @@ if __name__ == '__main__':
     optimizer = torch.optim.Adam(GNN_model.parameters(), lr=0.01, weight_decay=5e-4)
 
     GNN_model.train()
-
+    loss_history = []
     pbar = tqdm.tqdm(total=data_size)
     for epoch in range(epochs):
-        avg_loss = 0
+        epoch_total_loss = 0
         for batch in loader:
             x, A, A_weights, y = batch.x, batch.edge_index, batch.weights, batch.y
             optimizer.zero_grad()
             y_hat = GNN_model(x, A, A_weights=A_weights)
             loss = F.mse_loss(y_hat, y)
             loss.backward()
-            avg_loss += loss.item()
+            epoch_total_loss += loss.item()
             optimizer.step()
 
-        pbar.set_description(f'Epoch {epoch:02d}, Loss {avg_loss / data_size}')
+        epoch_loss = epoch_total_loss / epochs
+        pbar.set_description(f'Epoch {epoch:02d}, Loss {epoch_loss}')
+        loss_history.append(epoch_loss)
 
+    plt.plot(loss_history)
+    plt.title("GNN Loss graph")
+    plt.yscale('log')
+    plt.xlabel("#Epoch")
+    plt.ylabel("Loss")
+    plt.show()
     loader = DataLoader(D.dataset, batch_size=1)
 
     alpha = 0.2
     GNN_model.eval()
+    opt_avg_cost = 0
     for sample in loader:
         x, A, A_weights, y = sample.x, sample.edge_index, sample.weights, sample.y
 
@@ -94,12 +104,12 @@ if __name__ == '__main__':
             A_weights.requires_grad = True
 
             cost = GNN_model(x, A, A_weights)
-            GNN_model.zero_grad()
-
             grad = torch.autograd.grad(cost, A_weights)[0]
             A_weights = A_weights + alpha*grad[0]
             A_weights = A_weights.detach()
 
             print(cost.item())
-        pass
+        opt_avg_cost += cost.item()
+
+    print("####### {0} #######".format(opt_avg_cost))
 
